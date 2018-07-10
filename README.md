8f867b8a1c7bf7e7143f9651f8d902dbff5f137f
JasonG-FR <jason.gombert@protonmail.com>
2018-07-10 16:30:57 +0200

    First implementation, only top 10 cryptos are displayed

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # pyCoin
 A python script (CLI) for displaying crypto currencies data from CMC in a terminal
+
+```
+  Rank  Symbol    Name            Price (USD)    24h-Change    7d-Change    24h-Volume  (USD)
+------  --------  ------------  -------------  ------------  -----------  -------------------
+     1  BTC       Bitcoin           6391.6600        -6.04%       -3.11%        4,155,810,000
+     2  ETH       Ethereum           439.7960        -9.55%       -6.53%        1,774,530,000
+     3  XRP       XRP                  0.4497        -5.98%       -9.19%          230,668,000
+     4  BCH       Bitcoin Cash       699.0210        -7.01%      -10.08%          403,471,000
+     5  EOS       EOS                  7.3888       -14.13%      -17.36%          803,152,000
+     6  LTC       Litecoin            76.2079        -7.71%      -11.94%          312,492,000
+     7  XLM       Stellar              0.1956        -7.74%       -7.72%           37,662,700
+     8  ADA       Cardano              0.1308        -9.35%      -17.27%           63,652,300
+     9  MIOTA     IOTA                 0.9714       -10.24%      -16.88%           51,676,600
+    10  USDT      Tether               1.0005        -0.91%        0.07%        3,127,410,000
+```
---
+++ pyCoin.py
@@ -0,0 +1,120 @@
+import requests
+from tabulate import tabulate
+
+
+class Crypto(object):
+    def __init__(self, data):
+        self.id = data["id"]
+        self.name = data["name"]
+        self.symbol = data["symbol"]
+        self.website_slug = data["website_slug"]
+
+    def get_ticker(self, convert="USD"):
+        url = f"https://api.coinmarketcap.com/v2/ticker/{self.id}/?convert={convert}"
+        r = requests.get(url, timeout=10)
+
+        if r.status_code == 200:
+            # Add the ticker value from the JSON
+            ticker = r.json()["data"]
+            self.rank = ticker["rank"]
+            self.price = ticker["quotes"][convert]["price"]
+            self.percent_change_24h = ticker["quotes"][convert]["percent_change_24h"]
+            self.percent_change_7d = ticker["quotes"][convert]["percent_change_7d"]
+            self.volume_24h = ticker["quotes"][convert]["volume_24h"]
+            self.currency = convert
+        else:
+            raise ConnectionError(f"{url} [{r.status_code}]")
+
+    def set_ticker(self, ticker, convert):
+        self.rank = ticker["rank"]
+        self.price = ticker["quotes"][convert]["price"]
+        self.percent_change_24h = ticker["quotes"][convert]["percent_change_24h"]
+        self.percent_change_7d = ticker["quotes"][convert]["percent_change_7d"]
+        self.volume_24h = ticker["quotes"][convert]["volume_24h"]
+        self.currency = convert
+
+
+class bcolors:
+    PINK = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+
+
+def bold(text):
+    return bcolors.BOLD + str(text) + bcolors.ENDC
+
+
+def color(text, color):
+    colors = {"p": bcolors.PINK, "b": bcolors.BLUE, "g": bcolors.GREEN, "y": bcolors.YELLOW, "r": bcolors.RED}
+    return colors[color] + str(text) + bcolors.ENDC
+
+
+def color_percent(value):
+    if value < 0:
+        return color(value / 100, "r")
+    else:
+        return color(value / 100, "g")
+
+
+def load_cmc_ids():
+    # Get the JSON file from CMC API : https://api.coinmarketcap.com/v2/listings/
+    url = "https://api.coinmarketcap.com/v2/listings/"
+    r = requests.get(url, timeout=10)
+
+    if r.status_code == 200:
+        # Parse the JSON into a dict of Crypto objects
+        return {data["symbol"]: Crypto(data) for data in r.json()["data"]}
+    else:
+        raise ConnectionError(f"{url} [{r.status_code}]")
+
+
+def get_top_10(cryptos, convert="USD"):
+    url = f"https://api.coinmarketcap.com/v2/ticker/?limit=10&convert={convert}"
+    r = requests.get(url, timeout=10)
+
+    if r.status_code == 200:
+        # Parse the JSON and update the Crypto objects
+        data = r.json()['data']
+        selected = []
+        for key in data:
+            cryptos[data[key]["symbol"]].set_ticker(data[key], convert)
+            selected.append(cryptos[data[key]["symbol"]])
+
+        return selected
+    else:
+        raise ConnectionError(f"{url} [{r.status_code}]")
+
+
+def print_selection(selection):
+    # Generate a list of tuple containing the data to print
+    to_print = []
+    for item in selection:
+        data = (bold(item.rank), item.symbol, item.name, item.price, color_percent(item.percent_change_24h),
+                color_percent(item.percent_change_7d), item.volume_24h)
+        to_print.append(data)
+
+    currency = selection[0].currency
+    headers = ["Rank", "Symbol", "Name", f"Price ({currency})", "24h-Change",
+               "7d-Change", f"24h-Volume  ({currency})"]
+    headers = [bold(h) for h in headers]
+    floatfmt = ("", "", "", ".4f", ".2%", ".2%", ",.0f")
+    print(tabulate(to_print, headers=headers, floatfmt=floatfmt))
+
+
+def main():
+    # Load the crypto ids from CMC
+    cryptos = load_cmc_ids()
+
+    # Get the tickers of the top 10 cryptos
+    selection = get_top_10(cryptos)
+
+    # Print the selection
+    print_selection(selection)
+
+
+if __name__ == '__main__':
+    main()
